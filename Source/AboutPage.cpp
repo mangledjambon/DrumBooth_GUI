53f6dfcedc4e4b21ed47f0d654f3a419fac6fad2
Sean Breen <seanbreen91@gmail.com>
2016-04-29 15:22:33 +0100

    updated About application

--- Source/AboutPage.cpp
+++ Source/AboutPage.cpp
@@ -16,7 +16,11 @@
 
 AboutPage::AboutPage()
 {
-	setSize(300, 300);
+	setSize(500, 400);
+
+	logo = ImageFileFormat::loadFrom(File::getCurrentWorkingDirectory().getChildFile("../../Source/res/logo.png"));
+	imageComponent.setImage(logo);
+	addAndMakeVisible(imageComponent);
 }
 
 AboutPage::~AboutPage()
@@ -24,14 +28,26 @@ AboutPage::~AboutPage()
 
 void AboutPage::paint(Graphics& g)
 {
-	g.fillAll(Colours::whitesmoke);   // fill the component with a colour
+	// 0xd311645 = CIT red
+	g.fillAll(Colour(211, 17, 69));   // fill the component with a colour
 
+	Rectangle<int> localBounds = getLocalBounds();
+	Rectangle<int> logoArea = localBounds.removeFromTop(getHeight()/2).reduced(5);
+	imageComponent.setBounds(logoArea);
+
+	Rectangle<int> textArea = localBounds;
+	g.setColour(Colours::whitesmoke);
+	g.fillRect(textArea.toFloat().reduced(5));
 	g.setColour(Colours::darkslategrey);
-	g.setFont(14.0f);
+	g.setFont(20.0f);
 	g.drawFittedText(JUCEApplication::getInstance()->getApplicationName()
-		+ " v" + JUCEApplication::getInstance()->getApplicationVersion()
-		+ "\nSean Breen, R00070693\nDCOM4 Final Year Project 2016\nCork Institute of Technology", getLocalBounds(),
-		Justification::centred, true);
+		+ " v" + JUCEApplication::getInstance()->getApplicationVersion() 
+		+ "\nDeveloped by Sean Breen\nDCOM4 Final Year Project 2016",textArea.reduced(10), Justification::centredTop, 1);
+
+	g.setFont(16.0f);
+	g.drawFittedText("\nDrumBooth is a free, open-source software application for Windows which enables the user to isolate and separate the harmonic and percussive components from an audio file.\n\nSeparation algorithm adapted from Derry Fitzgerald's technique described in his paper 'Harmonic-Percussive Separation Using Median Filtering', DaFX10.", 
+		textArea.removeFromBottom(128).reduced(10),
+		Justification::horizontallyJustified, true);
 }
 
 void AboutPage::resized()
--- Source/AboutPage.h
+++ Source/AboutPage.h
@@ -21,6 +21,8 @@ public:
 	void resized() override;
 
 private:
+	Image logo;
+	ImageComponent imageComponent;
 
 };
 
